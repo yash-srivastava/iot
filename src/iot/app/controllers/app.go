ab4946e950419bd0b78ba6975f56bfffaf3da65e
yash-sri <yash.srivastava@homebell.com>
2017-06-07 12:39:50 +0530

    added json params

--- src/iot/app/controllers/app.go
+++ src/iot/app/controllers/app.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"iot/lib/sender"
-	"iot/lib/utils"
+	"encoding/json"
 )
 
 type App struct {
@@ -18,19 +18,13 @@ func (c App) Index() revel.Result {
 func (c App) Send_3000() revel.Result {
 
 	response := sender.Response{Success: false, Message:"Something went wrong"}
+	packet := sender.Packet_3000{}
 
-	params := sender.Packet_3000{}
-	params.SguId = utils.ToUint64(c.Params.Form.Get("sgu_id"))
-	params.ScuId = utils.ToUint64(c.Params.Form.Get("scu_id"))
-	params.GetSet =utils.ToInt(c.Params.Form.Get("get_set"))
-	params.Pwm = utils.ToInt(c.Params.Form.Get("pwm"))
-	params.Op1 = utils.ToInt(c.Params.Form.Get("op1"))
-	params.Op2 = utils.ToInt(c.Params.Form.Get("op2"))
-	params.Op3 = utils.ToInt(c.Params.Form.Get("op3"))
-	params.Op4 = utils.ToInt(c.Params.Form.Get("op4"))
-	params.Retry = utils.ToInt(c.Params.Form.Get("retry"))
-	params.RetryDelay = utils.ToInt(c.Params.Form.Get("retry_delay"))
+	params := c.Params.JSON
+	err := json.Unmarshal(params, &packet)
+	if err==nil{
+		response = sender.HandlePacket(0x3000, packet)
+	}
 
-	response = sender.HandlePacket(0x3000, params)
 	return c.RenderJSON(response)
 }
