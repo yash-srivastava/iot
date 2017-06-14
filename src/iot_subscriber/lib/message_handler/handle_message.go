3fa5489928038fe5806547fcddb095a6201f2073
yash-sri <yash.srivastava@homebell.com>
2017-06-14 22:01:26 +0530

    added 8001

--- src/iot_subscriber/lib/message_handler/handle_message.go
+++ src/iot_subscriber/lib/message_handler/handle_message.go
@@ -16,15 +16,18 @@ func Handle(input interface{})  {
 	packet_type := utils.ToInt((data["packet_type"].Value))
 	revel.INFO.Println("packet_type=>",packet_type)
 	db := dbutils.DBCONN
+
+	//Update Status of SGU
+
+	sgu := dbutils.Sgu{}
+	incoming_sgu := dbutils.Sgu{}
+	incoming_sgu.Sgu_id = utils.ToUint64(data["incoming_sgu_id"].Value)
+
+	db.Where(incoming_sgu).Assign(dbutils.Sgu{Is_connected: 1}).FirstOrCreate(&sgu)
+
 	switch packet_type {
 
 	case 0x003: {
-		sgu := dbutils.Sgu{}
-		incoming_sgu := dbutils.Sgu{}
-		incoming_sgu.Sgu_id = utils.ToUint64(data["incoming_sgu_id"].Value)
-
-		db.Where(incoming_sgu).Assign(dbutils.Sgu{Is_connected: 1}).FirstOrCreate(&sgu)
-
 		iterate := utils.ToInt(data["iterate"].Value)
 
 		incoming_scu := dbutils.Scu{}
@@ -43,12 +46,6 @@ func Handle(input interface{})  {
 	}
 
 	case 0x3001:{
-		sgu := dbutils.Sgu{}
-		incoming_sgu := dbutils.Sgu{}
-		incoming_sgu.Sgu_id = utils.ToUint64(data["incoming_sgu_id"].Value)
-
-		db.Where(incoming_sgu).Assign(dbutils.Sgu{Is_connected: 1}).FirstOrCreate(&sgu)
-
 		incoming_scu := dbutils.Scu{}
 
 		incoming_scu.Scu_id = utils.ToUint64(data["scuid"].Value)
@@ -62,10 +59,6 @@ func Handle(input interface{})  {
 	}
 
 	case 0xe000:{
-		sgu := dbutils.Sgu{}
-		incoming_sgu := dbutils.Sgu{}
-		incoming_sgu.Sgu_id = utils.ToUint64(data["incoming_sgu_id"].Value)
-
 		db.Where(incoming_sgu).Assign(dbutils.Sgu{Is_connected: 1}).FirstOrCreate(&sgu)
 
 		iterate := utils.ToInt(data["iterate"].Value)
@@ -83,6 +76,24 @@ func Handle(input interface{})  {
 				db.Where(incoming_scu).Assign(dbutils.Scu{Sgu_id: sgu.Sgu_id, Is_connected: 1}).FirstOrCreate(&scu)
 			}
 		}
+	}
+	case 0x8001:{
+		incoming_scu := dbutils.Scu{}
+
+		incoming_scu.Scu_id = utils.ToUint64(data["scuid"].Value)
+		scu := dbutils.Scu{}
+		db.Where(incoming_scu).Assign(dbutils.Scu{Sgu_id: sgu.Sgu_id, Is_connected: 1}).FirstOrCreate(&scu)
+
+		att_schedule := dbutils.Attached_Schedules{}
+		att_schedule.Scheduling_id = utils.ToInt(data["scheduling_id"].Value)
+		att_schedule.Pwm = utils.ToInt(data["pwm"].Value)
+		att_schedule.Scu_id = incoming_scu.Scu_id
+
+		expr := utils.ToStr(data["expression"].Value)
+
+		saved_schedule := dbutils.Attached_Schedules{}
+
+		db.Where(att_schedule).Assign(dbutils.Attached_Schedules{Expression: expr, Status_updated_at: time.Now()}).FirstOrCreate(&saved_schedule)
 	}
 
 	}
